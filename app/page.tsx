228abc0e879f7ece2eb9cea00afdd051a3d169df
Samuel <JS.Rosemond@gmail.com>
2023-04-28 23:16:28 -0400

    update solution due to warning (cleared)

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,65 @@
-import ImageCard from "./components/ImageCard";
+import ImageCard, { ImageCardProp } from "./components/ImageCard";
+
+const getStaticImage = async () => {
+  /**
+   * solution below
+   * https://www.youtube.com/watch?v=pfHjxyeCHRs&t=5269s
+   */
+  const imageArray: ImageCardProp[] = [
+    {
+      image: "/taylor-smith-Nike-unsplash.jpg",
+      brand: "Nike",
+      idName: "Nike_label",
+      altText: "A Pair of red white and black Nike shoes",
+    },
+    {
+      image: "/ervan-new-balance-unsplash.jpg",
+      brand: "New Balance",
+      idName: "NB_label",
+      altText: "A pair of Grey white dark grey accented New Balance Sneakers",
+    },
+    {
+      image: "/rachael-converse-unsplash.jpg",
+      brand: "Converse",
+      idName: "Converse_label",
+      altText: "Classic black and white Converse sneakers",
+    },
+    {
+      image: "/mayur-reebok-unsplash.jpg",
+      brand: "Reebok",
+      idName: "Reebok_label",
+      altText: "Black Reebok sneaker overlaid with white stripes",
+    },
+    {
+      image: "/the-puma-unsplash.jpg",
+      brand: "Puma",
+      idName: "Puma_label",
+      altText: "Classic white Puma with black label on the side",
+    },
+    {
+      image: "/tom-fila-unsplash.jpg",
+      brand: "Fila",
+      idName: "Fila_label",
+      altText: "Classic white Fila sneakers",
+    },
+  ];
+
+  const formattedImageArray = await Promise.all(
+    imageArray.map((el) => {
+      const formattedImage = require(`../public${el.image}`);
+      el = { ...el, image: formattedImage.default };
+
+      return el;
+    })
+  );
+  return formattedImageArray;
+};
 
 export default async function Home() {
-  const mapImageCard = async () => {
-    const imageArray = [
-      {
-        image: require(`../public/taylor-smith-Nike-unsplash.jpg`),
-        brand: "Nike",
-        idName: "Nike_label",
-        altText: "A Pair of red white and black Nike shoes",
-      },
-      {
-        image: require(`../public/ervan-new-balance-unsplash.jpg`),
-        brand: "New Balance",
-        idName: "NB_label",
-        altText: "A pair of Grey white dark grey accented New Balance Sneakers",
-      },
-      {
-        image: require(`../public/rachael-converse-unsplash.jpg`),
-        brand: "Converse",
-        idName: "Converse_label",
-        altText: "Classic black and white Converse sneakers",
-      },
-      {
-        image: require(`../public/mayur-reebok-unsplash.jpg`),
-        brand: "Reebok",
-        idName: "Reebok_label",
-        altText: "Black Reebok sneaker overlaid with white stripes",
-      },
-      {
-        image: require(`../public/the-puma-unsplash.jpg`),
-        brand: "Puma",
-        idName: "Puma_label",
-        altText: "Classic white Puma with black label on the side",
-      },
-      {
-        image: require(`../public/tom-fila-unsplash.jpg`),
-        brand: "Fila",
-        idName: "Fila_label",
-        altText: "Classic white Fila sneakers",
-      },
-    ];
+  const imageCards = await getStaticImage();
 
-    return imageArray.map(async (el) => {
-      /**
-       require can wrap around imagePath of obj[] like above
-       alternative solutions below
-       const image = require(`../public${el.image}`);
-       image={image} or third solution at youtube link
-       https://www.youtube.com/watch?v=pfHjxyeCHRs&t=5269s */
-
+  const mapImageCard = (): any | Promise<JSX.Element>[] => {
+    return imageCards.map(async (el) => {
       return (
         <ImageCard
           key={el.brand}
